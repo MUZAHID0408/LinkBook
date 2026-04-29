e5937984268dfba921445b3f75ba85855a2010a5
MUZAHID0408 <mdmuzahid0408@gmail.com>
2026-04-29 20:26:55 +0600

    uibuild

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 const morgan = require("morgan");
 app.use(express.json());
 app.use(express.static("dist"));
-app.use(cors());
 
 morgan.token("reqData", (request) => {
   //console.log(request.body);
