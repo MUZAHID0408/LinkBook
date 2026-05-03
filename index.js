e7675b9318305fdb81665573ba92742dada5659f
MUZAHID0408 <mdmuzahid0408@gmail.com>
2026-05-04 00:33:35 +0600

    uibuild

--- index.js
+++ index.js
@@ -44,39 +44,48 @@ app.get("/", (request, response) => {
   response.send("<h1>THE PHONEBOOK BACKEND<h1/>");
 });
 
-app.get("/api/persons", (request, response) => {
-  Persons.find({}).then((persons) => {
-    response.json(persons);
-  });
+app.get("/api/persons", (request, response, next) => {
+  Persons.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
   const currentTime = new Date().toString();
-  Persons.countDocuments({}).then((count) => {
-    response.send(
-      `Phonebook has info for ${count} people </br> ${currentTime}`,
-    );
-  });
+  Persons.countDocuments({})
+    .then((count) => {
+      response.send(
+        `Phonebook has info for ${count} people </br> ${currentTime}`,
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  Persons.find(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id
+  Persons.findById(id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+//Completed 3.15
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   Persons.findByIdAndDelete(id)
     .then(() => {
       response.status(204).end();
     })
     .catch((error) => {
-      response.status(400).json({ error: error.message });
+      next(error);
     });
 });
 
-app.post("/api/persons", (request, response) => {
+//create a person
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
   if (!body.name && !body.number) {
     response
@@ -93,10 +102,40 @@ app.post("/api/persons", (request, response) => {
     .then((savedPerson) => {
       response.json(savedPerson);
     })
-    .catch((error) =>
-      response.status(400).send(`Something went wrong. ${error.message}`),
-    );
+    .catch((error) => next(error));
 });
+
+//update a person number
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+  const id = request.params.id;
+
+  Persons.findByIdAndUpdate(
+    id,
+    { name, number },
+    { returnDocument: "after", runValidators: true, context: "query" },
+  )
+    .then((updatedPerson) => response.json(updatedPerson))
+    .catch((error) => next(error));
+});
+
+//Some MIDDLEWARES
+
+const handleUnknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "Unknown endpoint!!!" });
+};
+app.use(handleUnknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error);
+  if (error.message === "castError") {
+    return response.status(400).send({ error: "Malformed ID" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
